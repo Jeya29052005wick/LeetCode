589b0272638644325219fe183b5c5586c98a268d
Jeyachandran <jeyawick2005@gmail.com>
2025-08-26 19:16:23 +0530

    Initial commit - added first solved problems

---
+++ Palindrome Number.java
@@ -0,0 +1,28 @@
+class Solution {
+  public static boolean isPalindrome(int x)
+  {
+    if(x<0)
+    {
+        return false;
+    }
+    String str = String.valueOf(x);
+    int left = 0;
+    int right = str.length() - 1;
+
+    while(left<right)
+    {
+        if(str.charAt(left) != str.charAt(right))
+        {
+            return false;
+        }
+        left ++;
+        right --;
+    }
+    return true;
+   }
+
+   public static void main(String args[])
+   {
+    System.out.println(isPalindrome(121));
+   }
+}
